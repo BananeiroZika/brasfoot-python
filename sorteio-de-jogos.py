b3f9832f36336deb61340a0c12925ed6a20bb114
BananeiroZika <51134379+BananeiroZika@users.noreply.github.com>
2019-05-29 08:55:41 -0300

    Update sorteio-de-jogos.py
    Coloquei funções para comparar os resultados
    Não sei o motivo de aparecer 'none' depois do return

--- sorteio-de-jogos.py
+++ sorteio-de-jogos.py
@@ -1,26 +1,35 @@
 import random
 lista = []
-print(lista)
+times = ['Barcelona' ,'São Paulo', 'Milan', 'River Plate']
+print(times[0])
+#vou tentar fazer a adição de numeros a matriz utilizando função
+def sorteio(a, b):
+
+    resp = random.randint(a, b)
+    return resp
+
+def comparar(golstime1, golstime2,nomedotime):
+    if golstime1 > golstime2:
+        return print('O', nomedotime, 'Venceu')
+    elif golstime1 < golstime2:
+        return print('O', nomedotime, 'PERDEU')
+    else:
+        return "Empate"
+
+timea = sorteio(0, 9)
+print(timea)
+
 contador = 0
 while contador < 4:
     lista.append(random.randint(0, 9))
     contador = contador + 1
 
-print(lista)
-
-print('TIME A', lista[0], 'X', lista[1], 'Time B')
-
-if lista[0] > lista[1]:
-    print('TIME A VENCEU')
-elif lista[0] < lista[1]:
-    print('TIME B VENCEU')
-else:
-    print('EMPATE')
-
-print('Time C', lista[2], 'X',lista[3],'Time D')
-if lista[2] > lista[3]:
-        print('TIME C GANHOU')
-elif lista[2] < lista[3]:
-        print('TIME D GANHOU')
-else:
-        print('EMPATE')
+                                                    # JOGOS
+#JOGO 1
+print(times[0], timea, 'X', lista[1], times[1])
+    #COMPARAR RESULTADOS
+print(comparar(timea, lista[1],times[0]))
+#JOGO 2
+print(times[2], lista[2], 'X',lista[3], times[3])
+    #COMPARAR RESULTADOS
+print(comparar(lista[2], lista[3], times[2]))
