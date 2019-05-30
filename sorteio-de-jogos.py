04b9fbda2d633359745dc62250f91a0368dfcf25
BananeiroZika <51134379+BananeiroZika@users.noreply.github.com>
2019-05-30 10:23:35 -0300

    Update sorteio-de-jogos.py
    Coloquei um "BANCO DE DADOS" com um .csv que vai armazenar os times

--- sorteio-de-jogos.py
+++ sorteio-de-jogos.py
@@ -1,35 +1,60 @@
 import random
 lista = []
-times = ['Barcelona' ,'São Paulo', 'Milan', 'River Plate']
-print(times[0])
-#vou tentar fazer a adição de numeros a matriz utilizando função
-def sorteio(a, b):
+times = []
+timesjogando = []
+arquivo = open("times.csv", "r")
+times = arquivo.readlines()
+arquivo.close()
+tam = len(times)
+for i in range(tam):
+    jogar = times[i]
+    print(jogar.strip('\n'))
+    timesjogando.append(jogar.strip('\n'))
+    print(timesjogando)
 
-    resp = random.randint(a, b)
+#vou tentar fazer a adição de numeros a matriz utilizando função
+def sorteio():
+
+    resp = random.randint(0, 5)
     return resp
 
 def comparar(golstime1, golstime2,nomedotime):
     if golstime1 > golstime2:
-        return print('O', nomedotime, 'Venceu')
+        return print('O ', nomedotime, 'Venceu')
     elif golstime1 < golstime2:
         return print('O', nomedotime, 'PERDEU')
     else:
         return "Empate"
 
-timea = sorteio(0, 9)
-print(timea)
 
 contador = 0
 while contador < 4:
     lista.append(random.randint(0, 9))
     contador = contador + 1
 
-                                                    # JOGOS
-#JOGO 1
-print(times[0], timea, 'X', lista[1], times[1])
-    #COMPARAR RESULTADOS
-print(comparar(timea, lista[1],times[0]))
-#JOGO 2
-print(times[2], lista[2], 'X',lista[3], times[3])
-    #COMPARAR RESULTADOS
-print(comparar(lista[2], lista[3], times[2]))
+timea = sorteio()
+timeb = sorteio()
+timec = sorteio()
+timed = sorteio()
+final1 = sorteio()
+final2 = sorteio()
+# JOGOS
+print('='*15,'SEMIFINAL:','='*15)
+print(timesjogando[1],timea,'X',timeb, timesjogando[0])
+print(timesjogando[2],timec,'X',timed,timesjogando[3])
+if timea > timeb:
+    finalista1 = timesjogando[1]
+elif timea == timeb:
+    finalista1 = timesjogando[0]
+else:
+    finalista1 = timesjogando[0]
+
+if timec > timed:
+    finalista2 = timesjogando[2]
+elif timec == timed:
+    finalista2 = timesjogando[3]
+else:
+    finalista2 = timesjogando[3]
+
+print('='*15,'FINAL:','='*15)
+print(finalista2, final1 ,'X', final2, finalista1)
